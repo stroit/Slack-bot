9229cfc305eb5b7d9b11dda7a711ebec031fe53b
Philip YoonShin <wbvcos@gmail.com>
2017-02-03 15:41:14 -0500

    Revert "Extract slack token to using dotenv (.env files)"

--- slackbot.js
+++ slackbot.js
@@ -1,22 +1,20 @@
-require('dotenv').config();
-
 var Bot = require('slackbots');
-const moment = require('moment-timezone'),
-      rp = require('request-promise'),
-      bodyParser = require('body-parser'),
-      CronJob = require('cron').CronJob;
+const moment = require('moment-timezone');
+const rp = require('request-promise');
+const bodyParser = require('body-parser');
+const CronJob = require('cron').CronJob;
 
 let settings = {
-    token: process.env.SLACK_TOKEN,
-    name: process.env.SLACK_CHANNEL_NAME
+    token: 'xoxb-130645201734-sBS02yRe3p8EK7A6MbhMAXuf',
+    name: 'reminder'
 };
 
 bodyParser.json();
 moment.tz.setDefault("EST");
 
 var Bot = new Bot(settings);
-Bot.on('start', function () {
-    console.log("[!] bot just started");
+Bot.on('start', function() {
+	console.log("[!] bot just started");
     let papa = {
         "attachments": [{
             "color": "#3498db",
@@ -26,34 +24,34 @@ Bot.on('start', function () {
             ]
         }]
     };
-
+    
     let gifLinks = [];
     let giphyUrl = "http://api.giphy.com/v1/gifs/search?q=excited&api_key=dc6zaTOxFJmzC"
 
     rp(giphyUrl)
-        .then(function (htmlString) {
+        .then(function(htmlString) {
             var parsedJson = JSON.parse(htmlString);
             for (var i = 0; i < 25; i++) {
                 gifLinks.push(parsedJson.data[i].images.downsized.url);
             }
         })
-        .finally(function () {
-            var job = new CronJob({
-                cronTime: '00 00 00 * * 0-6',
-                onTick: function () {
-                    let randNum = Math.floor(Math.random() * 25 + 1);
-                    Bot.postMessageToChannel('general', gifLinks[randNum], papa);
-                },
-                start: true,
-                timeZone: 'EST'
-            });
-            //job.start();
+        .finally(function() {
+	        var job = new CronJob({
+		        cronTime: '00 00 00 * * 0-6',
+		        onTick: function() {
+			        let randNum = Math.floor(Math.random() * 25 + 1);
+			        Bot.postMessageToChannel('general', gifLinks[randNum], papa);
+			    },
+			    start: true,
+			    timeZone: 'EST'
+			});
+			//job.start();
         })
-        .catch(function (err) {
+        .catch(function(err) {
             Bot.postMessageToChannel('general', "Bot Error!");
         })
-})
-
+})	
+	
 function text() {
     let now = moment().dayOfYear();
     let day = moment("20170207").dayOfYear();
